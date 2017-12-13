67e87d1ba1ef871d14e39ac86a3f3cb53a7b7b4a
Jason C. Leach <jason.leach@fullboar.ca>
2017-12-13 14:10:56 -0800

    Project org

--- SecureImage/AppDelegate.swift
+++ SecureImage/AppDelegate.swift
@@ -1,9 +1,21 @@
 //
-//  AppDelegate.swift
-//  SecureImage
+// SecureImage
 //
-//  Created by Jason Leach on 2017-12-13.
-//  Copyright © 2017 Pathfinder. All rights reserved.
+// Copyright © 2017 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Created by Jason Leach on 2017-12-13.
 //
 
 import UIKit
@@ -12,7 +24,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -40,7 +51,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+     // MARK:
 }
-
