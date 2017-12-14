cdf4c17a11f7c785af7ee4e8c39d455773464745
Jason C. Leach <jason.leach@fullboar.ca>
2017-12-13 17:09:17 -0800

    Initial work on albums

---
+++ SecureImage/UI/Albums/AlbumsViewController.swift
@@ -0,0 +1,54 @@
+//
+// SecureImage
+//
+// Copyright © 2017 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Created by Jason Leach on 2017-12-13.
+//
+
+import UIKit
+
+class AlbumsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let createFirstAlbumView: CreateFirstAlbumView = {
+        let v = Bundle.main.loadNibNamed("CreateFirstAlbumView", owner: self, options: nil)?.first as! CreateFirstAlbumView
+        v.translatesAutoresizingMaskIntoConstraints = false
+        
+        return v
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        commonInit()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    private func commonInit() {
+        tableView.isHidden = true
+        
+        view.addSubview(createFirstAlbumView)
+        
+        createFirstAlbumView.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        createFirstAlbumView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        createFirstAlbumView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        createFirstAlbumView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
