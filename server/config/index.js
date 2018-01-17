549e83e1ab5195e5f478c9c09eb5f86ccc52bfe0
Jason C. Leach <jason.leach@fullboar.ca>
2018-01-17 11:15:44 -0800

    Support .env file for config

--- server/config/index.js
+++ server/config/index.js
@@ -24,8 +24,13 @@
 
 import path from 'path';
 import nconf from 'nconf';
+import dotenv from 'dotenv';
 
 const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  dotenv.config();
+}
 
 /**
  * These settings contain sensitive information and should not be
