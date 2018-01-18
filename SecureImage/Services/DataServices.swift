1510ab356fc9c1ecc5045b4f50c14e7c24a7ff00
Jason C. Leach <jason.leach@fullboar.ca>
2018-01-18 10:40:19 -0800

    Implement delete album

--- SecureImage/Services/DataServices.swift
+++ SecureImage/Services/DataServices.swift
@@ -172,6 +172,27 @@ class DataServices: NSObject {
         return nil
     }
     
+    internal class func remove(album: Album) -> Bool {
+
+        guard let realm = try? Realm() else {
+            print("Unable open realm")
+            return false
+        }
+        
+        if let myAlbum = realm.objects(Album.self).filter("id == %@", album.id).first {
+            do {
+                try realm.write {
+                    realm.delete(myAlbum);
+                }
+                return true
+            } catch {
+                fatalError("Unable to write to realm")
+            }
+        }
+        
+        return false
+    }
+    
     internal class func canAddToAlbum(album: Album) -> Bool {
         
         guard let realm = try? Realm() else {
