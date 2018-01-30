a51dcfc9f18f13325d34a7afd2185f299cb820d4
Jason C. Leach <jason.leach@fullboar.ca>
2018-01-30 14:04:42 -0800

    Complete test coverage for bucket helpers

--- test/bucket_spec.js
+++ test/bucket_spec.js
@@ -62,13 +62,13 @@ describe('minio bucket helpers', function() {
 
   test('listBucket returns an array of JSON objects', async () => {
     const stream = new PassThrough()
-    stream.end();
-
     const stub = sinon.stub(minio.Client.prototype, 'listObjectsV2').returns(stream);
+
+    stream.end();
 
     bucket.client = stub;
 
-    const objects = await bucket.listBucket('foo', 'bar');
+    const objects = await bucket.listBucket('aBucketName', 'aObjectName');
 
     stream.emit('data', object1);
     stream.emit('data', object1);
@@ -78,7 +78,7 @@ describe('minio bucket helpers', function() {
     expect(objects[0]).toEqual(object1); // contents are JSON   
   });
 
-  test('putObject returns a JSON object with etag property or throws', async () => {
+  test('putObject returns a JSON object with etag property or Error', async () => {
     const stub = sinon.stub(minio.Client.prototype, 'putObject');
     const error = new Error('Hello World');
     stub.onFirstCall().yields(undefined, object2);
@@ -86,12 +86,66 @@ describe('minio bucket helpers', function() {
 
     bucket.client = stub;
 
-    const result = await bucket.putObject('foo', 'bar', new Buffer(32));
+    const result = await bucket.putObject('aBucketName', 'aObjectName', new Buffer(32));
 
     expect(typeof result).toBe('object'); // return type array
     expect(result).toEqual(object2); // contents are JSON   
     expect(result).toHaveProperty('etag') // has correct props
 
-    await expect(bucket.putObject('foo', 'bar', new Buffer(32))).rejects.toEqual(error);
+    await expect(bucket.putObject('aBucketName', 'aObjectName', new Buffer(32))).rejects.toEqual(error);
+  });
+
+  test('getObject returns a Buffer of expected data', async () => {
+    const str1 = 'hello world';
+    const str2 = 'hello jello';
+    const stream = new PassThrough()
+    const stub = sinon.stub(minio.Client.prototype, 'getObject')
+
+    stream.push(str1);
+    stream.push(str2);
+    stream.end();
+
+    stub.onFirstCall().yields(undefined, stream);
+
+    bucket.client = stub;
+
+    const result = await bucket.getObject('aBucketName', 'aObjectName');
+
+    expect(typeof result).toBe('object'); // return type object
+    expect(result.length).toEqual((str1 + str2).length); // size is OK
+  });
+
+  test('getPresignedUrl returns URL in string format or Error', async () => {
+    const url = 'http://example.com/';
+    const stub = sinon.stub(minio.Client.prototype, 'presignedUrl');
+    const error = new Error('Hello World');
+    stub.onFirstCall().yields(undefined, url);
+    stub.onSecondCall().yields(error, undefined);
+
+    bucket.client = stub;
+
+    const result = await bucket.getPresignedUrl('aBucketName', 'aObjectName');
+
+    expect(typeof result).toBe('string'); // return type array
+    expect(result).toEqual(url); // contents are string   
+
+    await expect(bucket.getPresignedUrl('aBucketName', 'aObjectName')).rejects.toEqual(error);
+  });
+
+  test('removeObject retunrs correctly for success and Error', async () => {
+    const stub = sinon.stub(minio.Client.prototype, 'removeObject');
+    const error = new Error('Hello World');
+    stub.onFirstCall().yields(undefined, undefined);
+    stub.onSecondCall().yields(error, undefined);
+
+    bucket.client = stub;
+
+    const result = await bucket.removeObject('aBucketName', 'aObjectName');
+
+    // success
+    expect(typeof result).toBe('undefined');
+
+    // fails
+    await expect(bucket.removeObject('aBucketName', 'aObjectName')).rejects.toEqual(error);
   });
 });
