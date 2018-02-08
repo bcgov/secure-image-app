f386615af6405202631ca1e42a6b12cd696ecc08
Aidan Laing <aidan@freshworks.io>
2018-02-08 15:31:06 -0800

    Added Auth Spec

---
+++ test/auth_spec.js
@@ -0,0 +1,51 @@
+//
+// SecureImage
+//
+// Copyright © 2018 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Created by Jason Leach on 2018-02-01.
+//
+
+/* eslint-env es6 */
+
+'use strict';
+
+import jest from 'jest';
+import sinon from 'sinon';
+
+const PassThrough = require('stream').PassThrough;
+
+describe('archive functions', function() {
+
+    beforeEach(() => {
+      // nothig to do
+    });
+  
+    afterEach(() => {
+      // nothig to do
+    });
+  
+    test.skip('authenticate() returns an access token', async () => {
+
+    });
+
+    test.skip('introspection() completes without errors', async () => {
+
+    });
+
+    test.skip('isAuthenticated() completes without errors', async () => {
+
+    });
+});
