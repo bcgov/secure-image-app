b37a1ec9df7fabef5e6b2aa1c8d1e2d99444f08c
Jason C. Leach <jason.leach@fullboar.ca>
2018-02-08 13:51:01 -0800

    Work on authenticaiton mechanics

--- server/libs/auth.js
+++ server/libs/auth.js
@@ -39,52 +39,59 @@ const authenticate = () => new Promise((resolve, reject) => {
 });
 
 const introspection = (apiAccessToken, clientAccessToken) => new Promise((resolve, reject) => {
-  request.post('https://dev-sso.pathfinder.gov.bc.ca/auth/realms/mobile/protocol/openid-connect/token/introspect', {
-    form: {
-      grant_type: 'client_credentials',
-      client_id: 'secure-image-api',
-      client_secret: '0e6e0200-d003-46bd-8f5c-035736e94e1f',
-    },
-  }, (err, res, introspectionBody) => {
-    if (err) {
-      reject(err);
-      return;
-    }
+  
+  const error = new Error('Something went wrong');
+  error.code = 401;
+  reject(error);
+  
+  // resolve(true);
+  
+  // request.post('https://dev-sso.pathfinder.gov.bc.ca/auth/realms/mobile/protocol/openid-connect/token/introspect', {
+  //   form: {
+  //     grant_type: 'client_credentials',
+  //     client_id: 'secure-image-api',
+  //     client_secret: '0e6e0200-d003-46bd-8f5c-035736e94e1f',
+  //   },
+  // }, (err, res, introspectionBody) => {
+  //   if (err) {
+  //     reject(err);
+  //     return;
+  //   }
 
-    const errorDescription = JSON.parse(introspectionBody).error_description;
-    if (errorDescription != null) {
-      const error = new Error(errorDescription);
-      error.code = 401;
-      reject(error);
-      return;
-    }
+  //   const errorDescription = JSON.parse(introspectionBody).error_description;
+  //   if (errorDescription != null) {
+  //     const error = new Error(errorDescription);
+  //     error.code = 401;
+  //     reject(error);
+  //     return;
+  //   }
 
-    resolve();
-  });
+  //   resolve();
+  // });
 });
 
 // eslint-disable-next-line import/prefer-default-export
-export const isAuthenticated = (req, res, next) => {
-  // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
-  // you can do this however you want with whatever variables you set up
-  const clientAccessToken = req.header('access_token');
-  if (clientAccessToken != null) {
-    authenticate()
-      .then((apiAccessToken) => {
-        console.log('a here');
-        return introspection(apiAccessToken, clientAccessToken);
-      })
-      .then(() => {
-        console.log('b here');
-        next(null, true);
-      })
-      .catch((err) => {
-        console.log('e here');
-        res.status(401).json({ error: err.message });
-        // errorHandler(err);
-      });
-    return;
-  }
+export const isAuthenticated = async (req, res, next) => {
+  // if (!req.headers.authorization) {
+  //   return res.status(403).json({ error: 'No credentials sent.' });
+  // }
 
-  errorHandler(new Error('No access token passed'));
+  const clientAccessToken = ''; // funciton to parse out token....
+
+  try {
+    const apiAccessToken = await authenticate();
+    const results = await introspection(apiAccessToken, clientAccessToken);
+
+    if (results) {
+      next();
+    }
+  } catch (err) {
+    logger.error(`authentication error = ${err.message}`);
+
+    return res.status(401).json({ error: err.message, success: false });
+
+    // const error = new Error('Unauthorized');
+    // error.code = 401;
+    // throw error;
+  }
 };
