ffc23169f05bec2d2c669a57e49a0210b48d9a14
Jason C. Leach <jason.leach@fullboar.ca>
2020-08-21 12:00:42 -0700

    Fix probe endpoint

--- api/src/router/index.js
+++ api/src/router/index.js
@@ -36,7 +36,7 @@ const corsOptions = {
 
 module.exports = (app) => {
   app.use(cors(corsOptions));
-  app.use('/ehlo', ehlo); // probes
+  app.use('/v1/ehlo', ehlo); // probes
   app.use('/v1/auth', auth);
   app.use('/v1/album', album);
 };
