0bcac860b2381b6483ee8c838c7d22ae8dde5d7b
Amir Shayegh <shayegh@me.com>
2022-03-23 12:21:44 -0700

    fix for ios pipeline

--- .github/scripts/archive_adhoc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -workspace ios/SecureImage.xcworkspace -scheme SecureImage -sdk iphoneos -configuration AppStoreDistribution archive -archivePath $PWD/build/SecureImage.xcarchive clean archive
