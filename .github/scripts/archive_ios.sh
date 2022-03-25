5d7b81551c40dab50bedec4f7af8b73d12233674
Amir Shayegh <shayegh@me.com>
2022-03-25 00:07:03 -0700

    ios workflow update

--- .github/scripts/archive_ios.sh
+++ .github/scripts/archive_ios.sh
@@ -2,4 +2,4 @@
 
 set -eo pipefail
 
-sudo xcodebuild -workspace $PWD/ios/SecureImage.xcworkspace -scheme SecureImage -sdk iphoneos -configuration AppStoreDistribution archive -archivePath $PWD/build/SecureImage.xcarchive clean archive
+xcodebuild -workspace $PWD/ios/SecureImage.xcworkspace -scheme SecureImage -sdk iphoneos -configuration AppStoreDistribution archive -archivePath $PWD/build/SecureImage.xcarchive clean archive
--- .github/scripts/export_ios_ipa.sh
+++ .github/scripts/export_ios_ipa.sh
@@ -2,4 +2,4 @@
 
 set -eo pipefail
 
-sudo xcodebuild clean -exportArchive -archivePath $PWD/build/SecureImage.xcarchive -exportPath $PWD/build -allowProvisioningUpdates -exportOptionsPlist ios/exportOptions-adhoc.plist xcpretty
+xcodebuild clean -exportArchive -archivePath $PWD/build/SecureImage.xcarchive -exportPath $PWD/build -allowProvisioningUpdates -exportOptionsPlist ios/exportOptions-adhoc.plist xcpretty
